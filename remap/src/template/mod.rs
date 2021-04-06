7c8b6d562ca784ec9a54831e6a57201b3f0aa2a1
tuztu <tuztucode@gmail.com>
2021-04-06 23:45:30 +0800

    Insert & transaction

--- remap/src/template/mod.rs
+++ remap/src/template/mod.rs
@@ -1,3 +1,18 @@
 pub mod mysql;
 pub mod postgres;
 pub mod sqlite;
+
+// For public trait hide function.
+pub(crate) mod private {
+    // Once again, we introduce an effectively
+    // private type to encode locality.
+    // This time, we make it uninhabited so we
+    // *cannot* accidentally leak it.
+    pub enum Local {}
+
+    // However, we pair it with a 'sealed' trait
+    // that is *only* implemented for `Local`.
+    pub trait IsLocal {}
+
+    impl IsLocal for Local {}
+}
--- remap/src/template/mysql.rs
+++ remap/src/template/mysql.rs
@@ -11,19 +11,15 @@ use crate::arguments::Args;
 use crate::extend::Remap;
 use sqlx::mysql::{MySqlArguments, MySqlQueryResult};
 use std::borrow::BorrowMut;
+use crate::template::private;
 
 static POOLS: OnceCell<HashMap<String, Pool<MySql>>> = OnceCell::new();
 
 #[async_trait]
 pub trait MySqlTemplate<S>: Debug where S: MySqlTemplate<S> {
 
-    async fn insert<T>(&self, t: &T) -> Result<u64, Error> where T : Remap<MySql> + Sync {
-        let holders = T::fields_name().iter().map(|_| "?").collect::<Vec<&str>>();
-        let sql = SqlBuilder::insert_into(T::table_name())
-            .fields(T::fields_name().as_slice())
-            .values(holders.as_slice())
-            .sql()?;
-
+    async fn insert<T>(&self, t: &T) -> Result<u64, Error> where T: Remap<MySql> + Sync {
+        let sql = Self::build_insert_sql::<private::Local, T>(1).sql()?;
         let mut args = MySqlArguments::default();
         t.fields_args().values.iter().for_each(|a| args.add(a));
 
@@ -35,33 +31,16 @@ pub trait MySqlTemplate<S>: Debug where S: MySqlTemplate<S> {
     }
 
     async fn insert_batch<'a, T>(&self, v: &Vec<&T>, tx: Option<&mut Transaction<'a, MySql>>)
-                                 -> Result<u64, Error>
-        where T : Remap<MySql> + Sync {
-        let mut sql = SqlBuilder::insert_into(T::table_name());
-        sql.fields(T::fields_name().as_slice());
-
-        let holders = T::fields_name().iter().map(|_| "?").collect::<Vec<&str>>();
+        -> Result<u64, Error>
+        where T: Remap<MySql> + Sync {
+        assert!(v.len() > 0);
+        let sql = Self::build_insert_sql::<private::Local, T>(v.len()).sql()?;
         let mut arguments = MySqlArguments::default();
         v.iter().for_each(|t| {
-            sql.values(holders.as_slice());
             t.fields_args().values.iter().for_each(|x| arguments.add(x));
         });
-        let sql = sql.sql()?;
 
-        let x = match tx {
-            Some(tx) => {
-                sqlx::query_with(sql.as_str(), arguments).execute(tx).await?
-            },
-            _ => {
-                let mut tx = self.pool().begin().await?;
-                let x = sqlx::query_with(sql.as_str(), arguments)
-                    .execute(&mut tx)
-                    .await?;
-                tx.commit().await?;
-                x
-            }
-        };
-
+        let x = self.insert_tx::<private::Local>(sql.as_str(), arguments, tx).await?;
         Ok(x.rows_affected())
 
         // `Bug and compile failed`
@@ -75,18 +54,79 @@ pub trait MySqlTemplate<S>: Debug where S: MySqlTemplate<S> {
         // tx.commit().await?;
     }
 
-    async fn insert_ignore<'a, T>(v: &Vec<T>, tx: &mut Transaction<'a, MySql>)
+    async fn insert_ignore<'a, T>(&self, v: &Vec<&T>, tx: Option<&mut Transaction<'a, MySql>>)
         -> Result<u64, Error>
-        where T : Remap<MySql> + Sync {
-        todo!()
+        where T: Remap<MySql> + Sync {
+        assert!(v.len() > 0);
+        let sql = Self::build_insert_sql::<private::Local, T>(v.len()).sql()?
+            .replace("INSERT", "INSERT IGNORE");
+        let mut arguments = MySqlArguments::default();
+        v.iter().for_each(|t| {
+            t.fields_args().values.iter().for_each(|x| arguments.add(x));
+        });
+
+        let x = self.insert_tx::<private::Local>(sql.as_str(), arguments, tx).await?;
+        Ok(x.rows_affected())
     }
 
     async fn insert_update<'a, T>(
-        v: &Vec<T>, set_fields: &[&str], args: Args<'a, MySql>, tx: &mut Transaction<'a, MySql>)
-        -> Result<(), Error>
-        where T : Remap<MySql> + Sync {
+        &self, v: &Vec<&T>, update_fields: &[&str], tx: Option<&mut Transaction<'a, MySql>>)
+        -> Result<u64, Error>
+        where T: Remap<MySql> + Sync {
+        assert!(v.len() > 0 && update_fields.len() > 0);
 
-        todo!()
+        let mut sql = Self::build_insert_sql::<private::Local, T>(v.len()).sql()?;
+        sql.pop(); // remove ;   sql.remove(sql.len() - 1);   sql.replace(";", "");
+
+        let mut update = String::new();
+        update_fields.iter().for_each(|x| {
+            if !update.is_empty() { update.push_str(","); }
+            let s = format!("{0} = new.{0}", x);
+            update.push_str(s.as_str());
+        });
+        let sql = format!("{} AS new ON DUPLICATE KEY UPDATE {};", sql, update);
+
+        let mut arguments = MySqlArguments::default();
+        v.iter().for_each(|t| {
+            t.fields_args().values.iter().for_each(|x| arguments.add(x));
+        });
+
+        let x = self.insert_tx::<private::Local>(sql.as_str(), arguments, tx).await?;
+        Ok(x.rows_affected())
+    }
+
+    #[doc(hidden)]
+    async fn insert_tx<'a, L: private::IsLocal>(
+        &self, sql: &str, arguments: MySqlArguments, tx: Option<&mut Transaction<'a, MySql>>)
+        -> Result<MySqlQueryResult, Error> {
+        let x = match tx {
+            Some(tx) => {
+                sqlx::query_with(sql, arguments).execute(tx).await?
+            },
+            _ => {
+                let mut tx = self.pool().begin().await?;
+                let x = sqlx::query_with(sql, arguments)
+                    .execute(&mut tx)
+                    .await?;
+                tx.commit().await?;
+                x
+            }
+        };
+        Ok(x)
+    }
+
+    #[doc(hidden)]
+    fn build_insert_sql<L, T>(size: usize) -> SqlBuilder
+        where L: private::IsLocal,
+              T: Remap<MySql> {
+        let mut sql = SqlBuilder::insert_into(T::table_name());
+        sql.fields(T::fields_name().as_slice());
+
+        let holders = T::fields_name().iter().map(|_| "?").collect::<Vec<&str>>();
+        for _ in 0..size {
+            sql.values(holders.as_slice());
+        }
+        sql
     }
 
     async fn update<'a, T>(set_fields: &[&str], and_where_eq: &[&str], args: Args<'a, MySql>)
@@ -135,12 +175,34 @@ mod example {
     use crate::template::mysql::MySqlTemplate;
 
     #[async_test]
-    async fn abc() {
+    async fn insert() {
         MySqlSource::init().await.unwrap();
-
-        let user = User {id: 2, name: "小猪".into()};
+        let user = User {id: 2, name: "Sam".into()};
         let a = MySqlSource::Default.insert(&user).await.unwrap();
     }
+
+    #[async_test]
+    async fn insert_batch() {
+        MySqlSource::init().await.unwrap();
+        let user_1 = User {id: 1, name: "Bob".into()};
+        let user_2 = User {id: 2, name: "Cat".into()};
+        let vec = vec![&user_1, &user_2];
+
+        let rows = MySqlSource::Default.insert_batch(&vec, None).await.unwrap();
+        assert_eq!(2, rows);
+        let rows = MySqlSource::Default.insert_ignore(&vec, None).await.unwrap();
+        assert_eq!(0, rows);
+
+        let user_2 = User {id: 2, name: "Cow".into()};
+        let user_3 = User {id: 3, name: "Dog".into()};
+        let vec = vec![&user_2, &user_3];
+        let rows = MySqlSource::Default.insert_update(&vec, &["name"], None).await.unwrap();
+        assert_eq!(3, rows)
+    }
+
+
+
+
 
     #[derive(Debug, Remap)]
     #[remap(sqlx::MySql, table = "user")]
